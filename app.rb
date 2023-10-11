50d5bf7d63a3adf0b809e9c0a054a1b1e59eaf49
adib-enc <adib35785@gmail.com>
2023-10-11 11:52:36 +0700

    wip json dump test

--- app.rb
+++ app.rb
@@ -1,73 +1,144 @@
 require 'sinatra'
 require 'json'
+require_relative 'veh.rb'
 
 # Calculator class to handle arithmetic operations
 class Calculator
-  def add(x, y)
-    x + y
-  end
+	def add(x, y)
+		x + y
+	end
 
-  def subtract(x, y)
-    x - y
-  end
+	def subtract(x, y)
+		x - y
+	end
 
-  def multiply(x, y)
-    x * y
-  end
+	def multiply(x, y)
+		x * y
+	end
 
-  def divide(x, y)
-    raise 'Division by zero is not allowed' if y == 0
-    x / y
-  end
+	def divide(x, y)
+		raise 'Division by zero is not allowed' if y == 0
+		x / y
+	end
 end
 
 calculator = Calculator.new
+
+def stdJson(s, m, d = nil)
+	{
+		success: s,
+		message: m,
+		data: d,
+	}
+end
+
+def success(m, d = nil)
+	stdJson(true, m, d)
+end
+
+def failed(m, d = nil)
+	stdJson(false, m, d)
+end
 
 # Define routes for each arithmetic operation
 
 # Addition
 get '/add/:x/:y' do
-  content_type :json
-  x = params[:x].to_f
-  y = params[:y].to_f
-  result = calculator.add(x, y)
-  { operation: 'addition', result: result }.to_json
+	content_type :json
+	x = params[:x].to_f
+	y = params[:y].to_f
+	result = calculator.add(x, y)
+	{ operation: 'addition', result: result }.to_json
 end
 
 # Subtraction
 get '/subtract/:x/:y' do
-  content_type :json
-  x = params[:x].to_f
-  y = params[:y].to_f
-  result = calculator.subtract(x, y)
-  { operation: 'subtraction', result: result }.to_json
+	content_type :json
+	x = params[:x].to_f
+	y = params[:y].to_f
+	result = calculator.subtract(x, y)
+	{ operation: 'subtraction', result: result }.to_json
 end
 
 # Multiplication
 get '/multiply/:x/:y' do
-  content_type :json
-  x = params[:x].to_f
-  y = params[:y].to_f
-  result = calculator.multiply(x, y)
-  { operation: 'multiplication', result: result }.to_json
+	content_type :json
+	x = params[:x].to_f
+	y = params[:y].to_f
+	result = calculator.multiply(x, y)
+	{ operation: 'multiplication', result: result }.to_json
 end
 
 # Division
 get '/divide/:x/:y' do
-  content_type :json
-  x = params[:x].to_f
-  y = params[:y].to_f
-  begin
-    result = calculator.divide(x, y)
-    { operation: 'division', result: result }.to_json
-  rescue StandardError => e
-    status 400
-    { error: e.message }.to_json
-  end
+	content_type :json
+	x = params[:x].to_f
+	y = params[:y].to_f
+	begin
+		result = calculator.divide(x, y)
+		{ operation: 'division', result: result }.to_json
+	rescue StandardError => e
+		status 400
+		{ error: e.message }.to_json
+	end
+end
+
+get '/veh/car' do
+	content_type :json
+	
+	begin
+		builder = Vehicle::VehicleBuilder.new("Car")
+		car = builder
+			.with_brand("Toyota")
+			.with_model("Camry")
+			.with_color("Blue")
+			.with_year(2023)
+			.with_engine("2.5L")
+			.with_transmission("Automatic")
+			.with_fuel_type("Gasoline")
+			.build
+
+		# puts car.to_s
+
+		# result = car
+		# success("Ok", car.to_json).to_json
+		success("Ok", JSON.dump(car)).to_json
+		# { message: 'division', result: result }.to_json
+	rescue StandardError => e
+		status 400
+		{ error: e.message }.to_json
+	end
+end
+
+get '/json1' do
+	content_type :json
+
+	begin
+		ruby_data = { "name" => "John", "age" => 30, "city" => "New York" }
+
+		# Encoding (Serializing) Ruby Data to JSON
+		json_string = JSON.dump(ruby_data)
+		puts "JSON String:"
+		puts json_string
+
+		# Decoding (Deserializing) JSON to Ruby Data
+		parsed_data = JSON.parse(json_string)
+		puts "\nParsed Data:"
+		puts parsed_data.inspect
+
+		success("ok", [
+			1,2,
+			json_string,
+			parsed_data,
+		]).to_json
+	rescue StandardError => e
+		status 400
+		failed(e.message , nil)
+	end
 end
 
 get '/' do
 	content_type :json
 	
 	{ msg: 'hello aa bb', result: [] }.to_json
-  end
+end
---
+++ veh.rb
@@ -0,0 +1,87 @@
+require 'json'
+
+class Vehicle
+	attr_accessor :vehicle_type, :brand, :model, :color, :year, :engine, :transmission, :fuel_type
+
+	def initialize(builder)
+		@vehicle_type = builder.vehicle_type
+		@brand = builder.brand
+		@model = builder.model
+		@color = builder.color
+		@year = builder.year
+		@engine = builder.engine
+		@transmission = builder.transmission
+		@fuel_type = builder.fuel_type
+	end
+
+=begin 
+def to_s
+	"Type: #{@vehicle_type}, Brand: #{@brand}, Model: #{@model}, Color: #{@color}, Year: #{@year}, Engine: #{@engine}, Transmission: #{@transmission}, Fuel: #{@fuel_type}"
+end 
+=end
+
+	# The VehicleBuilder class is responsible for building a Vehicle instance.
+class VehicleBuilder
+	attr_accessor :vehicle_type, :brand, :model, :color, :year, :engine, :transmission, :fuel_type
+
+	def initialize(vehicle_type)
+		@vehicle_type = vehicle_type
+	end
+
+	def with_brand(brand)
+		@brand = brand
+		self
+	end
+
+	def with_model(model)
+		@model = model
+		self
+	end
+
+	def with_color(color)
+		@color = color
+		self
+	end
+
+	def with_year(year)
+		@year = year
+		self
+	end
+
+	def with_engine(engine)
+		@engine = engine
+		self
+	end
+
+	def with_transmission(transmission)
+		@transmission = transmission
+		self
+	end
+
+	def with_fuel_type(fuel_type)
+		@fuel_type = fuel_type
+		self
+	end
+
+	def build
+		Vehicle.new(self)
+	end
+	end
+end
+
+# Usage
+=begin
+builder = Vehicle::VehicleBuilder.new("Car")
+car = builder
+	.with_brand("Toyota")
+	.with_model("Camry")
+	.with_color("Blue")
+	.with_year(2023)
+	.with_engine("2.5L")
+	.with_transmission("Automatic")
+	.with_fuel_type("Gasoline")
+	.build
+
+puts car.to_json
+# puts car.to_s
+=end
